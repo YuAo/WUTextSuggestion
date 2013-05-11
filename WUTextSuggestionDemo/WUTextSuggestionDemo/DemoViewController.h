207293a204a551d33574113cfd5fe50d18d166d4
YuAo <me@imyuao.com>
2013-05-11 17:46:58 +0800

    initial commit

---
+++ WUTextSuggestionDemo/WUTextSuggestionDemo/DemoViewController.h
@@ -0,0 +1,13 @@
+//
+//  DemoViewController.h
+//  WUTextSuggestionDemo
+//
+//  Created by YuAo on 5/11/13.
+//  Copyright (c) 2013 YuAo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DemoViewController : UIViewController
+
+@end
---
+++ WUTextSuggestionDemo/WUTextSuggestionDemo/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  WUTextSuggestionDemo
+//
+//  Created by YuAo on 5/11/13.
+//  Copyright (c) 2013 YuAo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
